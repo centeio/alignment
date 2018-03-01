984a33fdb7e5d71a1b0b8f267524985316c78db6
Higor Araújo dos Anjos <higor.araujo.anjos@gmail.com>
2018-03-01 16:58:46 +0000

    Global Alignment done showing both sequences aligned at the end
    Co-authored-by: centeio  <c.centeiojorge@outlook.pt>
    Co-authored-by: EAlmeida1  <eduarda.017@hotmail.com>

--- Global.py
+++ Global.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 ########################
 #### UTIL FUNCTIONS ####
@@ -62,6 +64,86 @@ def global_align(seq1, seq2, Parameters=Parameters()):
 
     print_matrix("_"+seq1,"_"+seq2,M)
 
+    return M
+
+def align(M,seq1,seq2,Parameters):
+    """
+    Esta funcao encontra o melhor aliamente seguindo o algoritmo a seguir:
+
+        0- verificar se a celula corrente esta na borda
+        1- obter os n vizinhos
+        2- obter o valor da celula corrente
+        3- Usar a formula
+               a) M[i][j-1] + Parameters.gap == atual ?
+               b) M[i-1][j-1] + Parameters.score(seq1[i-1], seq2[j-1]) == atual ?
+               c) M[i-1][j] + Parameters.gap == atual ?
+        4 - Criar strings alignedseq1 e alignedseq2
+        5 - caso a) (esquerda)
+             alignedseq2 + "_"
+             alignedseq1 + seq1[j-1]   #"_"+seq1
+             j = j-1
+        
+             caso b) (diagonal)
+              alignedseq1 + seq1[j-1]
+              alignedseq2 + seq2[i-1]
+              j = j-1
+              i = i-1
+        
+             caso c) (cima)
+              alignedseq1 + "_"
+              alignedseq2 + seq2[i-1]
+              i = i-1
+        6 - reverter a string
+
+    OBS: J É LINHA e I É COLUNA
+    """
+
+    # As duas sequencias
+    alignedseq1 = ""
+    alignedseq2 = ""
+
+    # Os ultimos indices
+    i = len(seq1)
+    j = len(seq2)
+    
+    while i!=0 and j!=0:
+
+        #Primeira linha ou esquerda
+        if j==0 or (M[i][j] == M[i-1][j] + Parameters.gap):
+            alignedseq2 = alignedseq2 + "-"
+            alignedseq1 = alignedseq1 + seq1[i-1]
+            i = i-1
+            continue
+        
+        #Primeira coluna
+        if i==0:
+            alignedseq1 = alignedseq1 + "-"
+            alignedseq2 = alignedseq2 + seq2[j-1]
+            j = j-1
+            continue
+
+        #Diagonal
+        if (M[i][j] == M[i-1][j-1] + Parameters.score(seq1[i-1], seq2[j-1])):
+            alignedseq2 = alignedseq2 + seq2[j-1]
+            alignedseq1 = alignedseq1 + seq1[i-1]
+            j = j-1
+            i = i-1
+            continue
+
+        #Cima
+        if (M[i][j] == M[i][j-1] + Parameters.gap):
+            alignedseq1 = alignedseq1 + '-'
+            alignedseq2 = alignedseq2 + seq2[j-1]
+            j=j-1
+            continue
+
+        
+    #Revertendo a String        
+    alignedseq1 = alignedseq1[::-1]
+    alignedseq2 = alignedseq2[::-1]
+
+    return alignedseq1, alignedseq2
+
 def print_matrix(seq1, seq2, M):
     """
     This function prints the matrix along with sequences
@@ -86,4 +168,31 @@ def print_matrix(seq1, seq2, M):
         print("")
 
 
-global_align("AACA", "AG", Parameters())
+
+if __name__ == '__main__':
+    #Definicoes dos parametros
+    par = Parameters(gap=-2,mismatch=-1, match=1)
+
+    #Sequencias
+    #>tr|B7Z8R9|B7Z8R9_HUMAN cDNA FLJ57474, highly similar to Homo sapiens plasticity
+    # related gene 3 (PRG-3), transcript variant 1, mRNA OS=Homo sapiens PE=2 SV=1
+    seq1 = "MAGTVLLAYYFECTDTFQVHIQGFFCQDGDLMKPYPGTEEESFITPLVLYCVLAATPTAI\
+    IFIGEISMYFIKSTRESLIAQEKTILTGECCYLNPLLRRIIRFTGVFAFGLFATDIFVNA\
+    GQVVTGHLTPYFLTVCKPNYTSADCQAHHQFINNGNICTGDLEVIEKARRSFPSKHAALS\
+    IYSALYATMYITSTIKTKSSRLAKPVLCLGTLCTAFLTGLNRVSEYRNHCSDVIAGFILG\
+    TAVALFLGMCVVHNFKGTQGSPSKPKPEDPRGVPLMAFPRIESPLETLSAQNHSASMTEVT"
+
+
+    #>sp|Q9Y2Y8|PRG3_HUMAN Proteoglycan 3 OS=Homo sapiens GN=PRG3 PE=1 SV=2
+    seq2 = "MQCLLLLPFLLLGTVSALHLENDAPHLESLETQADLGQDLDSSKEQERDLALTEEVIQAE\
+    GEEVKASACQDNFEDEEAMESDPAALDKDFQCPREEDIVEVQGSPRCKICRYLLVRTPKT\
+    FAEAQNVCSRCYGGNLVSIHDFNFNYRIQCCTSTVNQAQVWIGGNLRGWFLWKRFCWTDG\
+    SHWNFAYWSPGQPGNGQGSCVALCTKGGYWRRAQCDKQLPFVCSF"
+
+    #Matriz
+    matrix = global_align(seq1, seq2, par)
+    #Sequencias alinhadas
+    alignedseq1, alignedseq2 = align(matrix, seq1, seq2, par)
+
+    print(alignedseq1)
+    print(alignedseq2)
